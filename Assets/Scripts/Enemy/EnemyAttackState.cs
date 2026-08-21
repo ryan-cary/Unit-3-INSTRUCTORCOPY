5fdae661a40a9458b6220ada3e3110f74f3da8ba
scion <60977346+ryan-cary@users.noreply.github.com>
2026-08-20 20:18:37 -0700

    Added Enemy Attack State + Camera Pitch

--- Assets/Scripts/Enemy/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/EnemyAttackState.cs
@@ -2,23 +2,37 @@ using UnityEngine;
 
 public class EnemyAttackState : EnemyState
 {
-    public EnemyAttackState(Enemy enemy) : base(enemy)
-    {
+    float _distanceToPlayer;
 
-    }
+    public EnemyAttackState(Enemy enemy) : base(enemy)
+    {  }
 
     public override void OnStateEnter()
-    {
-        Debug.Log("Never should have come here!");
-    }
+    {  }
 
     public override void OnStateExit()
-    {
-
-    }
+    {  }
 
     public override void OnStateUpdate()
     {
+        if (_enemy.player != null)
+            _distanceToPlayer = Vector3.Distance(_enemy.transform.position, _enemy.player.position);
 
+        if (ShouldTransitionToFollow())
+            TransitionToFollow();
+
+        else
+            Attack();
     }
+
+    private void Attack()
+    { Debug.Log("Never should have come here!"); }
+
+    private bool ShouldTransitionToFollow()
+    {
+        return _enemy.player == null || _distanceToPlayer >= 2;
+    }
+
+    private void TransitionToFollow()
+    { _enemy.ChangeState(new EnemyFollowState(_enemy)); }
 }
---
+++ Assets/Scripts/Player/PlayerCameraBehaviour.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class PlayerCameraBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private Transform cameraPivot;
+
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float maximumAngle;
+    [SerializeField] private float minimumAngle;
+    private float verticalInput;
+
+    private float verticalRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+        TurnCamera();
+    }
+
+    void TurnCamera()
+    {
+        cameraPivot.localRotation = Quaternion.AngleAxis(verticalRotation, Vector3.right);
+    }
+
+    void UpdateRotation()
+    {
+        verticalRotation += verticalInput * verticalSpeed * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, minimumAngle, maximumAngle);
+    }
+
+    public void SetCameraVerticalInput(float value)
+    { verticalInput = value; }
+}
